2dbc3280cf139946a19855da4987b5257ad5a26f
dbcooper-peloton <daniel.cooper1@onepeloton.com>
2022-08-02 10:20:30 -0700

    V1

---
+++ Python Archive/NI_DAQ_Library.py
@@ -0,0 +1,24 @@
+import nidaqmx
+
+class NIDAQ:
+    '''
+    Library for using NIDAQ
+    Contains function to read voltage
+    '''
+    def readVoltage(device, channel):
+        '''
+        :param device: DAQ device number (ex: cDAQ1Mod3 for NI9215 since it's in the third spot)
+        :param channel: physical channel that sensor is connected to (ex: ai0)
+        :return: voltage reading from device/channel
+        '''
+        with nidaqmx.Task() as task:
+            task.ai_channels.add_ai_voltage_chan(device + "/" + channel)    #read voltage channel
+            output = task.read()    #voltage reading
+            #print(output)
+            return output
+
+
+    #print(readVoltage('cDAQ1Mod3', 'ai0'))
+
+
+
