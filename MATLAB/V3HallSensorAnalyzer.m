c4904656adad55ffcffa5ea960b0c22ed8b9224b
AndyKind <andy.kind@onepeloton.com>
2022-11-22 12:10:30 -0800

    V3 analyzer update

--- MATLAB/V3HallSensorAnalyzer.m
+++ MATLAB/V3HallSensorAnalyzer.m
@@ -1,6 +1,5 @@
-function newTable = V3HallSensorAnalyzer(fnameVector, plt)
+function newTable = V3HallSensorAnalyzer(~)
     
-    %comment these if calling from another script
     close all;clear;clc;
     fullfname = fullfile('C:\', 'Bike V3', 'Summary Report File 2022-11-10T11.33.56 P0.11_140rpm_5mm_sweep.csv');
     fullfname2 = fullfile('C:\', 'Bike V3', 'Summary Report File 2022-11-11T10.12.47 P0.11_140rpm_5mm_B2_v2.csv');
@@ -8,50 +7,60 @@ function newTable = V3HallSensorAnalyzer(fnameVector, plt)
     plt = false;
 
     %!!!!!!!!change these 2 variables to change sensor number/axis
-    activeSensor = 5; % 4, 5, or 6
-    activeAxis = 'x'; % 'x', 'y', 'z', or 'T'
+    activeSensor = [3, 4, 5];
+    activeAxis = {'x', 'y', 'z'};
 
     %some constants
     SnCell = 'B3:B3'; %SN cell
     PositionCategories = {'2mm', '7mm', '12mm', '17mm', '22mm', '27mm', '32mm'}; %Brake positions
+    fout = 'RheaHallSensorAnalysis.xlsx';
     
     %get board SNs
     SnVector = {};
     for sn= 1:length(fnameVector)
-        [~,bsn,~] = xlsread(fnameVector(sn), 1, SnCell);
-        SnVector{sn} = cell2mat(bsn);
+        [~,bsn,~] = xlsread(fnameVector(sn), 1, SnCell); %#ok<XLSRD> 
+        SnVector{sn} = cell2mat(bsn); %#ok<AGROW> 
     end
+    %!!!loop through sensor/axis combo.3x, 3y, 3z, 4x, 4y, 4z, 5x, 5y, 5z
 
-    %Get linear regression coefficents and create the models
-    RsquareVector = zeros([length(PositionCategories), 1]);
-    regressionVector = zeros([length(PositionCategories), 1]);
-    for lr=1:length(SnVector)
-        model = pltVsTorque(activeSensor, activeAxis, fnameVector(lr), plt);
-        regressionVector(:, lr) = model(:, 1);
-        RsquareVector(:, lr) = model(:, 2);
-       
-    end
-    
     %create an output table
-    tHeader = ["Position in MM"; "Board Sn"; "Slope"; "R Squared"; "Std"];
-    tTypes = ["string", "string", "double", "double", "double"];
+    tHeader = ["Position Category"; "Real Target Position"; "Board Sn"; "Sensor Number"; "Sensor Axis"; "Slope"; "R Squared"; "Std"];
+    tTypes = ["string", "string", "string", "string", "string", "double", "double", "double"];
     tSize = [length(SnVector) * length(PositionCategories), length(tHeader)];
     newTable = table('Size', tSize,'VariableTypes', tTypes, 'VariableNames', tHeader);
     posCounter = 1;
 
-    for z = 1:length(PositionCategories)
-        newReg = regressionVector(z, :);
-        newRS = RsquareVector(z, :);
-        Std = std(newReg);
-        for ez = 1:length(SnVector)
-            newTable(posCounter, :) = [PositionCategories(z) SnVector(ez) newReg(ez) newRS(ez) Std];
-            posCounter = posCounter + 1;
+    for senseNum = 1:length(activeSensor)
+        for senseAxis = 1:length(activeAxis)
+            %Get linear regression coefficents and create the models
+            RsquareVector = zeros([length(PositionCategories), 1]);
+            regressionVector = zeros([length(PositionCategories), 1]);
+            trueTargetPos = zeros([length(PositionCategories), 1]);
+            for lr=1:length(SnVector)
+                model = pltVsTorque(activeSensor(senseNum), activeAxis(senseAxis), fnameVector(lr), plt);
+                regressionVector(:, lr) = model(:, 1);
+                RsquareVector(:, lr) = model(:, 2);
+                trueTargetPos(:, lr) = model(:,3);
+            end
+
+            for z = 1:length(PositionCategories)
+                newReg = regressionVector(z, :);
+                newRS = RsquareVector(z, :);
+                Std = std(newReg);
+                for ez = 1:length(SnVector)
+                    newTable(posCounter, :) = [PositionCategories(z) trueTargetPos(z, ez) SnVector(ez) activeSensor(senseNum) activeAxis(senseAxis) newReg(ez) newRS(ez) Std];
+                    posCounter = posCounter + 1;
+                end
+            end
         end
     end
-    newTable.('Position in MM') = append(string(newTable.('Position in MM')), "mm"); %Func returns this
+    %!!!End interation
+
+    newTable.('Real Target Position') = append(string(newTable.('Real Target Position')), "mm"); %Func returns this
+    writetable(newTable, fout); %write to file
 
     %func to group data by target brake position, normalize, plot, and
-    %return a linear regression coefficent
+    %return a linear regression coefficent. plotData is a bool for plotting
     function SandR2 = pltVsTorque(sensorNum, sensorAxis, FileName, plotData)
         Data = readtable(FileName, "VariableNamingRule","preserve", NumHeaderLines=5);
         posCat = unique(Data.('Target Resistance'));
@@ -77,16 +86,14 @@ function newTable = V3HallSensorAnalyzer(fnameVector, plt)
                    'MarkerSize',3)
             end
         end
-        SandR2 = [linRegVector(:), Rsquare(:)];
+        SandR2 = [linRegVector(:), Rsquare(:), posCat(:)];
         if plotData
             grid on
             title(tName)
             xlabel('Dyno Torque')
-            ylabel('Magnetic Field uT')
-            
+            ylabel('Magnetic Field uT') 
             legend(poscatNames,...
                'location', 'northwest', NumColumns=1)
         end
     end
-
 end
