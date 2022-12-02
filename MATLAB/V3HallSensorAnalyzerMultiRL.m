e723f5a367bc40197b1e87d2d16612b6baca030c
AndyKind <andy.kind@onepeloton.com>
2022-12-02 15:48:24 -0800

    V3 changes

---
+++ MATLAB/V3HallSensorAnalyzerMultiRL.m
@@ -0,0 +1,140 @@
+function newTable = V3HallSensorAnalyzerMultiRL(~)
+
+close all;clear;clc;
+%format long
+
+%find all csv files in the dirPath and load the names into the fnameVector
+dirPath = 'C:\Bike V3';
+fnameVector = ["temp"]; % init the vector as string
+fileList = dir(fullfile(dirPath, '*.csv'));
+for f = 1:length(fileList)
+    fnameVector(f) = fullfile(dirPath, fileList(f).name);
+end
+
+%options
+plt = false; %toggle plot
+invertAxis = true; %invert the x/y axis
+
+%This script needs each file to have the same number of Position
+%Categories. The categorie names don't have to match between files.
+%To compare files that have differ num of categories, save truncated versions
+PositionCategories = {'0', '5', '10', '15', '20', '25', '30'};
+activeSensor = [3, 4, 5];
+activeAxis = ['x', 'y', 'z'];
+activeSide = ['R', 'L'];
+SnCell = 'B3:B3';
+
+fout = 'RheaHallSensorAnalysis_AllRL.xlsx';
+
+%get board SNs
+SnVector = {};
+for sn= 1:length(fnameVector)
+    [~,bsn,~] = xlsread(fnameVector(sn), 1, SnCell); %#ok<XLSRD>
+    SnVector{sn} = cell2mat(bsn); %#ok<AGROW>
+end
+
+%create an output table
+tHeader = ["Position Category"; "Real Target Position"; "Board Sn"; "Sensor Number"; "Sensor Axis"; "Sensor side"; "Slope"; "Constant"; "R Squared"; "Std"];
+tTypes = ["string", "string", "string", "string", "string", "string", "double", "double", "double", "double"];
+tSize = [length(SnVector) * length(PositionCategories), length(tHeader)];
+newTable = table('Size', tSize,'VariableTypes', tTypes, 'VariableNames', tHeader);
+
+%Get linear regression coefficents and create the models
+RsquareVector = zeros([length(PositionCategories), 1]);
+regressionVector = zeros([length(PositionCategories), 1]);
+trueTargetPos = zeros([length(PositionCategories), 1]);
+constantVector = zeros([length(PositionCategories), 1]);
+
+pCounter = 1;
+for lr=1:length(SnVector)
+    for s = 1:length(activeSide)
+        model = regmodel(activeSensor, activeAxis, fnameVector(lr), plt, SnVector(lr), invertAxis, activeSide(s));
+        regressionVector(:, pCounter) = model(:, 1);
+        RsquareVector(:, pCounter) = model(:, 2);
+        trueTargetPos(:, pCounter) = model(:, 3);
+        constantVector(:, pCounter) = model(:, 4);
+        pCounter = pCounter + 1;
+    end
+end
+
+%Write the Slope, Rsquared, and Standard Deviation to output file
+rCounter = 1;
+for pos = 1:length(PositionCategories)
+    posCounter = 1;
+    newReg = regressionVector(pos, :);
+    stdR = std(newReg(1:2:end));
+    stdL = std(newReg(2:2:end));
+    Std = [stdR, stdL];
+    newRS = RsquareVector(pos, :);
+    newconstant = constantVector(pos, :); 
+    for SN = 1:length(SnVector) 
+        for ss = 1:length(activeSide)
+            newTable(rCounter, :) = [PositionCategories(pos) trueTargetPos(pos, SN) SnVector(SN) activeSensor activeAxis activeSide(ss) newReg(posCounter) newconstant(posCounter) newRS(posCounter) Std(ss)];
+            posCounter = posCounter + 1;
+            rCounter = rCounter + 1;
+        end
+    end
+end
+
+%newTable.('Real Target Position') = append(string(newTable.('Real Target Position')), "mm");
+writetable(newTable, fout); %write to file
+
+%func to group data by target brake position, normalize, plot, and
+%return a linear regression coefficent. plotData is a bool for plotting
+    function RegressionModel = regmodel(sensorNum, sensorAxis, FileName, plotData, SN, inAx, side)
+        Data = readtable(FileName, "VariableNamingRule","preserve", NumHeaderLines=5);
+        posCat = unique(Data.('Target Resistance'));
+        ResVector = Data.('Target Resistance');
+        sensor = strcat(string(side), num2str(sensorNum), '-', string(sensorAxis));
+        pTitle = strcat(string(side), num2str(sensorNum), '-', string(sensorAxis));
+        linRegVector = zeros(size(posCat));
+        Rsquare = zeros(size(posCat));
+        constantsVector = zeros(size(posCat));
+        if plotData
+           figure
+        end
+
+        tName = strcat('Hall Sensor',{' '}, string(pTitle), {' '}, {'Board'}, {' '}, string(SN));
+
+        Data.H = Data.(sensor);
+        Data.NormalizedH = -(Data.H - Data.H(1));
+
+        for i = 1:length(posCat)
+            pos_df = Data(ResVector == posCat(i), :);
+            x = pos_df.('Torque Dyno');
+            y = pos_df.NormalizedH;
+            if inAx
+                tempTbl = table(y, x);
+            else
+                tempTbl = table(x, y);
+            end
+            
+            mdl = fitlm(tempTbl);
+            linRegVector(i) = mdl.Coefficients.Estimate(2);
+            constantsVector(i) = mdl.Coefficients.Estimate(1);
+            Rsquare(i) = mdl.Rsquared.Ordinary;
+            if plotData
+                hold on
+                p = plot(mdl);
+                delete(p(3:4));
+                delete(p(1));
+                scatter(y, x);
+            end
+        end
+        RegressionModel = [linRegVector(:), Rsquare(:), posCat(:), constantsVector(:)];
+        if plotData
+            grid on
+            title(tName)
+            if inAx
+                ylabel('Dyno Torque')
+                xlabel('Magnetic Field uT')
+            else
+                xlabel('Dyno Torque')
+                ylabel('Magnetic Field uT')
+            end
+            l = get(gca,'Children');
+            legend([l(1),l(3), l(5), l(7), l(9), l(11), l(13)],string(posCat));
+            exportgraphics(gcf,strcat(tName, '.png'),'Resolution',300);
+        end
+    end
+end
--- MATLAB/V3HallSensorAnalyzerSingleCombined.m
+++ MATLAB/V3HallSensorAnalyzerSingleCombined.m
@@ -1,7 +1,7 @@
-function newTable = V3HallSensorAnalyzerSingle(~)
+function newTable = V3HallSensorAnalyzerSingleCombined(~)
 
 close all;clear;clc;
-format long
+%format long
 
 %find all csv files in the dirPath and load the names into the fnameVector
 dirPath = 'C:\Bike V3';
@@ -11,8 +11,9 @@ for f = 1:length(fileList)
     fnameVector(f) = fullfile(dirPath, fileList(f).name);
 end
 
-%plot option
-plt = false;
+%options
+plt = true; %toggle plot
+invertAxis = true; %invert the x/y axis
 
 %This script needs each file to have the same number of Position
 %Categories. The categorie names don't have to match between files.
@@ -21,7 +22,8 @@ PositionCategories = {'0mm', '5mm', '10mm', '15mm', '20mm', '25mm', '30mm'};
 activeSensor = 4;
 activeAxis = 'x';
 SnCell = 'B3:B3';
-fout = 'RheaHallSensorAnalysisSingle.xlsx';
+
+fout = 'RheaHallSensorAnalysis_SingleCombined.xlsx';
 
 %get board SNs
 SnVector = {};
@@ -44,7 +46,7 @@ trueTargetPos = zeros([length(PositionCategories), 1]);
 constantVector = zeros([length(PositionCategories), 1]);
 
 for lr=1:length(SnVector)
-    model = regmodel(activeSensor, activeAxis, fnameVector(lr), plt, lr);
+    model = regmodel(activeSensor, activeAxis, fnameVector(lr), plt, lr, invertAxis);
     regressionVector(:, lr) = model(:, 1);
     RsquareVector(:, lr) = model(:, 2);
     trueTargetPos(:, lr) = model(:, 3);
@@ -69,46 +71,64 @@ writetable(newTable, fout); %write to file
 
 %func to group data by target brake position, normalize, plot, and
 %return a linear regression coefficent. plotData is a bool for plotting
-    function RegressionModel = regmodel(sensorNum, sensorAxis, FileName, plotData, SN)
+    function RegressionModel = regmodel(sensorNum, sensorAxis, FileName, plotData, SN, inAx)
         Data = readtable(FileName, "VariableNamingRule","preserve", NumHeaderLines=5);
         posCat = unique(Data.('Target Resistance'));
-        poscatNames = strcat(num2str(posCat), 'mm');
+        %poscatNames = strcat(num2str(posCat), 'mm') % for plot
         ResVector = Data.('Target Resistance');
         sensorR = strcat('R', num2str(sensorNum), '-', string(sensorAxis));
         sensorL = strcat('L', num2str(sensorNum), '-', string(sensorAxis));
         pTitle = strcat(num2str(sensorNum), '-', string(sensorAxis));
-        tName = strcat('Combined Hall Sensor',{' '}, string(pTitle), {' '}, {'Board'}, {' '}, string(SN));
         linRegVector = zeros(size(posCat));
         Rsquare = zeros(size(posCat));
         constantsVector = zeros(size(posCat));
-
         if plotData
-            figure
+           figure
         end
+
+        tName = strcat('Combined Hall Sensor',{' '}, string(pTitle), {' '}, {'Board'}, {' '}, string(SN));
+
+        Data.H = mean([Data.(sensorR), Data.(sensorL)], 2);
+        Data.NormalizedH = -(Data.H - Data.H(1));
+
         for i = 1:length(posCat)
             pos_df = Data(ResVector == posCat(i), :);
-            H = mean([pos_df.(sensorR), pos_df.(sensorL)], 2);
-            NormalizedH = -(H - H(1));
             x = pos_df.('Torque Dyno');
-            y = NormalizedH;
-            mdl = fitlm(x, y);
+            y = pos_df.NormalizedH;
+            if inAx
+                tempTbl = table(y, x);
+                %mName = strcat("uT ~ Toque", {' '}, poscatNames(i));
+            else
+                tempTbl = table(x, y);
+                %mName = strcat("Torque ~ uT", {' '}, poscatNames(i));
+            end
+            
+            mdl = fitlm(tempTbl);
             linRegVector(i) = mdl.Coefficients.Estimate(2);
             constantsVector(i) = mdl.Coefficients.Estimate(1);
             Rsquare(i) = mdl.Rsquared.Ordinary;
             if plotData
                 hold on
-                %plot(pos_df.('Torque Dyno'), NormalizedH, '-o',...
-                plot(mdl)
+                p = plot(mdl);
+                delete(p(3:4));
+                delete(p(1));
+                scatter(y, x);
             end
         end
         RegressionModel = [linRegVector(:), Rsquare(:), posCat(:), constantsVector(:)];
         if plotData
             grid on
             title(tName)
-            xlabel('Dyno Torque')
-            ylabel('Magnetic Field uT')
-            legend(poscatNames,...
-                'location', 'northwest', NumColumns=1)
+            if inAx
+                ylabel('Dyno Torque')
+                xlabel('Magnetic Field uT')
+            else
+                xlabel('Dyno Torque')
+                ylabel('Magnetic Field uT')
+            end
+            l = get(gca,'Children');
+            legend([l(1),l(3), l(5), l(7), l(9), l(11), l(13)],string(posCat));
+            exportgraphics(gcf,strcat(tName, '.png'),'Resolution',300);
         end
     end
 end
